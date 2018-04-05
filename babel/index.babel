f14184cb8924a81574f69fabccf9155d0432daec
Taiwo Ibidapo-Obe <Ibidapo-Obe>
2018-04-05 12:45:37 +0100

    Initial Commit

---
+++ babel/index.babel
@@ -0,0 +1,10 @@
+new Vue({
+  el: '#app',
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+      name: ''
+    }
+  }
+})
---
+++ js/index.js
@@ -0,0 +1,10 @@
+new Vue({
+  el: '#app',
+  data: function data() {
+    return {
+      num1: 0,
+      num2: 0,
+      name: ''
+    };
+  }
+});
